3d0a95ca3f18b4f0a45e483a4126197059b27e45
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-10-12 00:39:29 +0900

    s/string_contains/contains_string/ added docs

--- nanotap.h
+++ nanotap.h
@@ -6,20 +6,36 @@
 
 static int TEST_COUNT = 0;
 
+/**
+ * This simply evaluates any expression ("$got eq $expected" is just a
+ * simple example) and uses that to determine if the test succeeded or
+ * failed.  A true expression passes, a false one fails.  Very simple.
+ */
 static void ok(int x, const char *msg) {
     printf("%s %d - %s\n", (x ? "ok" : "not ok"), ++TEST_COUNT, msg ? msg : "");
 }
 
+/**
+ * display diagnostics message.
+ */
 static void diag(const char *msg) {
     fprintf(stderr, "# %s\n", msg ? msg : "");
 }
 
-static void string_contains(const char *got, const char *expected, const char *msg) {
-    ok(strstr(got, expected) != NULL, msg);
+/**
+ * contains_string() searches for $substring in $string.
+ */
+static void contains_string(const char *string, const char *substring, const char *msg) {
+    ok(strstr(string, substring) != NULL, msg);
 }
 
+/**
+ *  If you don’t know how many tests you’re going to run, you can issue
+ * the plan when you’re done running tests.
+ */
 static void done_testing() {
     printf("1..%d\n", TEST_COUNT);
+    exit(0);
 }
 
 #ifdef __cplusplus
@@ -27,10 +43,17 @@ static void done_testing() {
 
 #include <string>
 #include <iostream>
+
+/**
+ * shorthand for std::string
+ */
 static void diag(const std::string &msg) {
     diag(msg.c_str());
 }
 
+/**
+ * flexible is() based on C++ template.
+ */
 template <class T>
 static void is(T got, T expected, const char *msg) {
     if (got == expected) {
@@ -42,12 +65,18 @@ static void is(T got, T expected, const char *msg) {
     }
 }
 
+/**
+ * shorthand for lazy person
+ */
 static void ok(int x) {
     ok(x, "");
 }
 
-static void string_contains(const std::string &got, const char *expected, const char *msg) {
-    string_contains(got.c_str(), expected, msg);
+/**
+ * shorthand for std::string
+ */
+static void contains_string(const std::string &str, const char *substr, const char *msg) {
+    contains_string(str.c_str(), substr, msg);
 }
 #endif
 
--- t/02_cpp.cc
+++ t/02_cpp.cc
@@ -4,8 +4,8 @@ int main() {
     ok(1, "ok");
     ok(0, "ng");
     diag("hehe");
-    string_contains("yoyo", "ya", "not contains");
-    string_contains("yoyo", "yo", "contains");
+    contains_string("yoyo", "ya", "not contains");
+    contains_string("yoyo", "yo", "contains");
     
     // C++ specific
     is(1, 1, "good");
@@ -14,7 +14,7 @@ int main() {
     is(std::string("NG"), std::string("OK"), "bad");
     is(std::string("NG"), std::string("OK"), "bad");
     ok(1);
-    string_contains(std::string("YOaaa"), "aaa", "ok");
+    contains_string(std::string("YOaaa"), "aaa", "ok");
 
     done_testing();
 }
