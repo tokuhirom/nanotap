3d35e94a0f1cb83cfbb0cd9eb49b1966a6492613
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-10-28 00:27:04 +0900

    added author and license info

--- nanotap.pm
+++ nanotap.pm
@@ -28,3 +28,15 @@ see L<nanotap.h> itself :P
 =head1 SEE ALSO
 
 L<Test::More>
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom ah! gmail.comE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
