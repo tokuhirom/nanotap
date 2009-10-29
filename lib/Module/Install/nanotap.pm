19ef9faa705f12d05ae5b753625a3852da064784
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-10-30 01:47:44 +0900

    setup inc automatically

--- lib/Module/Install/nanotap.pm
+++ lib/Module/Install/nanotap.pm
@@ -10,12 +10,16 @@ use File::Spec;
 sub use_nanotap {
     my ($self) = @_;
 
+    $self->clib_setup();
+
     my ($header) = grep { -f $_ } map {
         File::Spec->catfile( $_, 'auto', 'Clib', 'include', 'nanotap',
             'nanotap.h' )
     } grep /$Config{archname}/, @INC;
     if ($header) {
         $self->admin->copy($header, File::Spec->catfile(qw/inc auto Clib include nanotap nanotap.h/));
+        my $incs = $self->makemaker_args->{INC} || '';
+        $self->makemaker_args->{INC} = "-Iinc/auto/Clib/include/ $incs";
     } else {
         die "cannot find nanotap.h";
     }
